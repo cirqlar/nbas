5ad26f59d31ff0e3762b13a63705677f5be53199
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2026-03-18 18:46:25 +0100

    rename to root

--- __tests__/Root.test.tsx
+++ __tests__/Root.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import Root from '../App';
+import Root from '../Root';
 
 test('renders correctly', async () => {
 	await ReactTestRenderer.act(() => {
