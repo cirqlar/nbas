d4bdcbc73bb0f130e91f861a987e3cb3d0d4c99c
Ibitoye Ayanfeoluwa <ibitoyeayanfeoluwa@gmail.com>
2026-03-18 18:33:50 +0100

    add stale and gc time to prevent unnecessary requests

--- src/queries/index.ts
+++ src/queries/index.ts
@@ -1,6 +1,13 @@
 import { QueryClient } from '@tanstack/react-query';
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			staleTime: 10 * 60 * 1000,
+			gcTime: 10 * 60 * 1000,
+		},
+	},
+});
 
 export default queryClient;
 
